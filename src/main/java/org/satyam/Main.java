f2cf8e4ca518fe06bbef2f12d87896b676ef5355
Satyam Suman <findsatyam95@gmail.com>
2023-03-15 00:01:13 +0530

    Tested both Imperative and Declarative Approach

--- src/main/java/org/satyam/Main.java
+++ src/main/java/org/satyam/Main.java
@@ -1,7 +1,57 @@
 package org.satyam;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("\nImperative Approach\n");
+        // Imperative Approach
+        List<Person> personList = List.of(
+                new Person("John", Gender.MALE),
+                new Person("Maria", Gender.FEMALE),
+                new Person("Ayesha", Gender.FEMALE),
+                new Person("Alex", Gender.MALE),
+                new Person("Alice", Gender.FEMALE)
+        );
+
+        List<Person> females = new ArrayList<>();
+
+        for (Person person : personList) {
+            if (Gender.FEMALE.equals(person.gender)) {
+                females.add(person);
+            }
+        }
+
+        for (Person female : females) {
+            System.out.println(female);
+        }
+
+        System.out.println("\nDeclarative Approach\n");
+        // Declarative Approach
+        personList.stream()
+                .filter(person -> Gender.FEMALE.equals(person.gender))
+                .toList()
+                .forEach(System.out::println);
+    }
+
+    enum Gender {
+        MALE, FEMALE
+    }
+
+    static class Person {
+        private final String name;
+        private final Gender gender;
+
+        Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" + "name='" + name + '\'' + ", gender=" + gender + '}';
+        }
     }
 }
